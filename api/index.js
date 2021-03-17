f569b54199874af8690a08a7e29c87703e5d062d
FrancoRosa <km115.franco@gmail.com>
2021-03-16 22:36:40 -0500

    Add upload file feature

--- api/index.js
+++ api/index.js
@@ -1,8 +1,14 @@
-const Joi = require('joi');
 const express = require('express');
+const fileUpload = require('express-fileupload');
+const Joi = require('joi');
 const app = express();
 
 app.use(express.json());
+app.use(fileUpload({
+  useTempFiles : true,
+  tempFileDir : '/tmp/',
+  debug: true,
+}));
 
 const courses = [
   { id: 1, name: 'course1'},
@@ -33,5 +39,26 @@ app.get('/api/courses/:id', (req,res) => {
   if (!course) res.status(404).send('not found')
   res.send(course);
 })
+
+app.post('/upload', function(req, res) {
+  let sampleFile;
+  let uploadPath;
+
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  sampleFile = req.files.sampleFile;
+  uploadPath = __dirname + '/uploaded/' + sampleFile.name;
+
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv(uploadPath, function(err) {
+    if (err)
+      return res.status(500).send(err);
+
+    res.send('File uploaded!');
+  });
+});
 
 app.listen(3000, () => console.log('...listening on 3000, great'))
