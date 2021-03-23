0e5cdb72bd5635c06cf3766c87a21151c126057a
FrancoRosa <km115.franco@gmail.com>
2021-03-22 20:59:45 -0500

    Refactor folder structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './darkly.css';
-import './index.css';
-import App from './App';
+import './css/darkly.css';
+import './css/index.css';
+import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
